67821df14fa6bb664f83ebe65c960567bf2fd958
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-08-23 12:52:02 -0600

    Add  Property to Temperatura

--- Expendedora.cs
+++ Expendedora.cs
@@ -8,8 +8,27 @@ namespace ExpendedoraG2_2024_I
         #region Atributos
         private string marca;
         private ushort cantproductos;
+        private byte temperatura;
         private float precio;
+
+
         #endregion
+
+        #region Propiedades
+        public byte Temperatura { 
+            get => temperatura;
+            set
+            {
+                if (0 < value && value < 25)
+                    temperatura = value;
+                else
+                    temperatura = 20;
+            }
+        }
+
+        #endregion
+
+
 
         #region Métodos
 
@@ -70,8 +89,16 @@ namespace ExpendedoraG2_2024_I
 
         public Expendedora(bool Mantenimiento)
         {
+            Temperatura = 1;
             if (  Mantenimiento == true)
              Console.WriteLine("Entrando en modo mantenimiento");
+            Console.WriteLine("Cambiando temperatura");
+            LimpiarDisplay();
+            for (int i =0; i < 20; i++)
+            {
+                Temperatura++;
+            }
+            Console.WriteLine("Mostrando Temperatura {0} [°C]",Temperatura );
         }
         #endregion
 
